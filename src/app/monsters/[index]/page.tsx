e9e267a8579a6ce7f439126b1569c7446cc4a8c9
David Cunningham <david@cunningdj.com>
2024-05-31 02:33:09 -0400

    Refactored Monster detail page, added more data

--- src/app/monsters/[index]/page.tsx
+++ src/app/monsters/[index]/page.tsx
@@ -2,10 +2,34 @@
 
 import { Card, CardMedia, Container, List, ListItem, ListItemText } from '@mui/material';
 import { NextPage } from 'next';
+import { FC } from 'react';
 
 import { getMonsterImagePath } from '@/components/DndApollo/dndApi';
 import { useMonsterQuery } from '@/components/Monster/queries';
-import { Body1, H2, Title } from '@/components/Typography';
+import { Body1, H2, H4, Title } from '@/components/Typography';
+
+
+const AttributeList: FC<{attributes:object, header:string}> = ({attributes, header}) => (
+  <>
+    <H2>{header}</H2>
+    <List>
+      {Object.entries(attributes).filter(([key,value]) => key !== '__typename' && value).map(([key, value]) => (
+        <ListItem key={key}><ListItemText><b>{key}</b>: {value}</ListItemText></ListItem>
+      ))}
+    </List>
+  </>
+);
+
+const NameDescriptionList: FC<{items: {name:string, desc:string}[], header:string}> = ({items, header}) => (
+  <>
+    <H2>{header}</H2>
+    <List>
+      {items.map(({name, desc}) => (
+        <ListItem key={name}><ListItemText><b>{name}</b>: {desc}</ListItemText></ListItem>
+      ))}
+    </List>
+  </>
+);
 
 
 const MonsterPage: NextPage<{params: { index:string }}> = ({ params: { index }}) => {
@@ -19,25 +43,19 @@ const MonsterPage: NextPage<{params: { index:string }}> = ({ params: { index }})
     <Container maxWidth="md">
       <Card>
         <Title>{monster.name}</Title>
+        <H4><b>{monster.size} {monster.type}</b>, {monster.alignment}</H4>
         <CardMedia image={image} component="img" />
         <Body1><b>Challenge Rating</b>: {monster.challenge_rating}</Body1>
         {/* <p>Armor Class: {monster.armor_class}</p> */}
         <Body1><b>Hit Points</b>: {monster.hit_points} ({monster.hit_dice})</Body1>
-        {/* <p>Speed: {monster.speed}</p> */}
         <Body1><b>STR</b>: {monster.strength} <b>DEX</b>: {monster.dexterity} <b>CON</b>: {monster.constitution} <b>INT</b>: {monster.intelligence} <b>WIS</b>: {monster.wisdom} <b>CHA</b>: {monster.charisma}</Body1>
         <Body1><b>Damage Vulnerabilities</b>: {monster.damage_vulnerabilities}</Body1>
         <Body1><b>Damage Resistances</b>: {monster.damage_resistances}</Body1>
         <Body1><b>Damage Immunities</b>: {monster.damage_immunities}</Body1>
         {/* <p>Condition Immunities: {monster.condition_immunities}</p> */}
-        {/* <p>Senses: {monster.senses}</p> */}
         <Body1><b>Languages</b>: {monster.languages}</Body1>
-        <H2>Speed</H2>
-        <List>
-          {Object.entries(monster.speed).filter(([key,value]) => key !== '__typename' && value).map(([key, value]) => (
-            <ListItem key={key}><ListItemText><b>{key}</b>: {value}</ListItemText></ListItem>
-          ))}
-        </List>
-        <H2>Proficiencies</H2>
+        <AttributeList header="Speed" attributes={monster.speed} />
+        <AttributeList header="Senses" attributes={monster.senses} />
         <List>
           {monster.proficiencies.map((proficiency) => (
             <ListItem key={proficiency.proficiency.name}><b>{proficiency.proficiency.name}</b>: {proficiency.value}</ListItem>
@@ -49,20 +67,9 @@ const MonsterPage: NextPage<{params: { index:string }}> = ({ params: { index }})
             <ListItem key={ability.name}><b>{ability.name}</b>: {ability.desc}</ListItem>
           ))}
         </List>
-        <H2>Actions</H2>
-        <List>
-          {monster.actions.map((action) => (
-            // <ListItem key={action.name}><b>{action.name}</b>: {action.desc}</ListItem>
-            <ListItem key={action.name}><ListItemText><b>{action.name}</b>: {action.desc}</ListItemText></ListItem>
-          ))}
-        </List>
-        <H2>Legendary Actions</H2>
-        <List>
-          {monster.legendary_actions.map((action) => (
-            <ListItem key={action.name}><ListItemText><b>{action.name}</b>: {action.desc}</ListItemText></ListItem>
-          ))}
-        </List>
-        <textarea style={{width: '95%', height: 600}}>{JSON.stringify(monster,null,2)}</textarea>
+        <NameDescriptionList header="Actions" items={monster.actions} />
+        <NameDescriptionList header="Legendary Actions" items={monster.legendary_actions} />
+        {/* <textarea style={{width: '95%', height: 600}}>{JSON.stringify(monster,null,2)}</textarea> */}
       </Card>
     </Container>
   ): (
